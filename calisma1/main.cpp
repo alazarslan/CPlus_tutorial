b0372d3d86a97a5d4da86bf97821e7ad5de18b30
Alaz Arslan <alazarslan5@gmail.com>
2024-07-08 22:43:31 +0300

    add: Polymorphism ve Inheritence
    inheritence ve polymorphism örnekleri, polymorphism'de tam olarak anlamadığım bir kısım kaldı

--- calisma1/main.cpp
+++ calisma1/main.cpp
@@ -62,6 +62,41 @@ public:
  * ortak (public) getter ve setter metotları kullanarak gerçekleştirilir.
  */
 
+// Inheritence ve Polymorphism
+
+class Animal {
+
+private: // private ve protected farkına bakmayı unutma!!!
+    int weight;
+public:
+    virtual void MakeNoise() { // virtual yazmamın sebebi bunun alt sınıflarda farklı bir şekilde kullanabilmeyi sağlamak, dog classa bak
+        cout << "Hayvan: An animal is making a noise" << endl;
+    }
+    int getWeight() {
+        return weight;
+    }
+    void setWeight(int num) {
+        weight = num;
+    }
+    void Eat() {
+        cout << "Animal is eating" << endl;
+    }
+};
+
+class Cat: public Animal {
+public:
+    void Miyav() {
+        cout << "Kedi: Miyav" << endl;
+    }
+};
+
+class Dog: public Animal {
+public:
+    void MakeNoise() {
+        cout << "Kopek: hav hav" << endl;
+    }
+};
+
 int main() {
     Car myCar;
     myCar.startCar();
@@ -71,6 +106,29 @@ int main() {
     Alaz.setStudentNum(109);
     cout << Alaz.getStudentName() << endl;
     cout << Alaz.getStudentNumber() << endl;
+    cout << "-----------------------------------------" << endl << endl;
+
+    Cat kedi;
+    kedi.setWeight(5); // setweight animal classı fonksiyonu
+    cout << kedi.getWeight() << endl;
+    kedi.Eat(); // eat fonksiyonu animal classındaydı fakat kedi kullandı
+    kedi.MakeNoise(); // makeNoise fonksiyonu da aynı şekilde
+    kedi.Miyav(); // bu zaten kedinin kendi fonksiyonu
+    // kedinin animal class'ı fonksiyonlarını kullabilmesi inheritence oluyor
+
+    cout << "-----------------------------------------" << endl << endl;
+
+    Animal *animal; // düzgün polymorphism için her zaman pointer kullan, düz variable kullanınca problem oluyor
+    // silip pointersiz düz variable ile dene ve farkı gör
+
+    Dog kopek;
+    kopek.MakeNoise();
+    kedi.MakeNoise(); // Hayvan: An animal is making a noise sonucu çıktı çünkü MakeNoise'yi köpekte yazdığım gibi yazmadım
+
+    //animal = kopek; // Hayvan: An animal is making a noise sonucu çıkıyor sebebini tam olarak bilmiyorum yarın sor!!!!
+    //animal.MakeNoise();
+    animal = &kopek;
+    animal->MakeNoise();
 
     return 0;
 }
